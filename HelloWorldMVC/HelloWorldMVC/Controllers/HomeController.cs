cfceec4b7e9ed9214297f6870d311392154df55a
mrkyaing <akog02@gmail.com>
2024-09-15 20:56:13 +0700

    etail

---
+++ HelloWorldMVC/HelloWorldMVC/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace HelloWorldMVC.Controllers
+{
+    public class HomeController:Controller
+    {
+        //host:port/home/sayhello?name="MG"&country=Myanmar
+        public string SayHello(string name,string country) => $"Hi,{name},Nice to see you!!,Are you from {country}?";
+        //Index is action Method
+        //host://port/home/index
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public ViewResult Welcome()
+        {
+            int hour = DateTime.Now.Hour;
+            string now = DateTime.Now.ToShortTimeString();
+            ViewBag.Greeting = hour < 12 ? $"Hello,Good Morning:{now}": $"Hello,Good Afternoon :{now}";
+            return View();
+        }
+        [HttpGet]
+        //host://port/home/order
+        public JsonResult Order()
+        {
+            var myOrder = new { orderId = "O001", orderName = "Shopping item", UnitPrice = 2000 };
+            return Json(myOrder);
+        }
+        //you need to send the data to n1 and n2 parameters as QueryString
+        //host://port/home/multiply?n1=10&n2=10
+        public int Multiply(int n1,int n2)
+        {
+            return n1 * n2;
+        }
+        //[NonAction]
+        public FileResult DownloadImage()
+        {
+            var fileName = "20yearsafterprogramming.png";
+            var filePath = Path.Combine("wwwroot/MyFiles/", fileName);
+            byte[] fileInByte=System.IO.File.ReadAllBytes(filePath);
+            return File(fileInByte,"text/png",fileName);
+        }
+        //host://port/home/iam
+        [ActionName("iam")]
+        public IActionResult DoMe()
+        {
+            return View("DoMe");
+        }
+
+        [HttpGet]
+        public int Add(int n1,int n2) => n1 + n2;//host://port/home/add  output : 2
+
+        [HttpPost]
+        public int Sum(int n1,int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
--- HelloWorldMVC/HelloWorldMVC/Program.cs
+++ HelloWorldMVC/HelloWorldMVC/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+//register Controllers & Views to the routing
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
+//define the default routing url  when project initialize in hosting
+//eg : server://host
+app.MapControllerRoute(name:"default", pattern: "{controller=Home}/{action=Index}/{id?}");
+//map all Controllers and Action Methods
+app.MapDefaultControllerRoute();
 app.Run();
