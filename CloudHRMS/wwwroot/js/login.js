6e67814eff75e48244baba508b0b60acc44ade6a
mrkyaing <akog02@gmail.com>
2024-11-01 10:48:29 +0700

    update with js file for login process.

---
+++ CloudHRMS/wwwroot/js/login.js
@@ -0,0 +1,17 @@
+﻿$("#logout").on('click', function (e) {
+    e.preventDefault(); // Prevent form submission
+
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You will be logged out!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, logout!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            document.getElementById('logoutForm').submit(); // Submit the form
+        }
+    });
+});
